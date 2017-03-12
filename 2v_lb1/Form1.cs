36c984dc2c05d791280dc544fc1eb13ece904cdc
mamishev201423 <mamishev201423@gmail.com>
2017-03-12 11:02:30 +0200

    first

---
+++ 2v_lb1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _2v_lb1
+{
+    public partial class Form1 : Form
+    {
+        string line;
+        string Filename = "text.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+                 
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            Metodth.WriteRandomNumber(Filename);  
+            //textBox1.Text = Metodth.ReadFile(Filename);
+            //label1.Text = Metodth.ReadFile(Filename);
+            using (StreamReader sr = new StreamReader("text.txt"))
+            {
+                line = sr.ReadToEnd();
+                textBox1.Text = line;                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {            
+            Metodth.Swap(Filename);
+            using (StreamReader sr = new StreamReader("text.txt"))
+            {
+                line = sr.ReadToEnd();
+                textBox1.Text = line;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ 2v_lb1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+using System.IO;
+
+namespace _2v_lb1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+     
+        static void Main(string[] args)
+        {
+            
+            string Filename = "text.txt";
+            //Metodth.WriteRandomNumber(Filename);
+           
+            //Metodth.Swap(Filename);
+            
+            //Metodth.ReadFile(Filename);            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+    }
+
+
+        public static class Metodth
+        {
+            public static double getRandomNumber(this Random rnd, double _min, double _max)
+            {
+                return rnd.NextDouble() * (_max - _min) + _min;
+            }
+
+            public static void WriteNumbers(string _filename, double[] _str)
+            {
+                using (StreamWriter writer = File.CreateText(_filename))
+                {
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        writer.WriteLine(_str[i]);
+                    }
+                }
+            }
+
+           public  static void WriteRandomNumber(string _filename)
+            {
+                using (StreamWriter writer = File.CreateText(_filename))
+                {
+                    Random rnd = new Random();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        writer.WriteLine(getRandomNumber(rnd, -5.00, 5.00));
+                        
+                    }             
+                }
+            }
+
+            //public static string ReadFile(string _filename)
+            //{
+            //    using (StreamReader reader = File.OpenText(_filename))
+            //    {
+            //        string line = null;
+            //        do
+            //        {
+            //            line = reader.ReadLine();
+                        
+            //        }
+            //        while (line != null);
+            //    return line;
+            //    }                
+            //}
+
+            public static void Swap(string _filename)
+            {
+                double[] number = new double[10];
+                double max, min;
+                using (StreamReader reader = File.OpenText(_filename))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        number[i] = Convert.ToDouble(reader.ReadLine());
+                    }
+
+                }
+                int index_max = 0, index_min = 0;
+                max = number[0]; min = number[0];
+                for (int i = 1; i < 10; i++)
+                {
+                    if (number[i] < min)
+                    {
+                        min = number[i];
+                        index_min = i;
+                    }
+                    else if (number[i] > max)
+                    {
+                        max = number[i];
+                        index_max = i;
+                    }
+                }
+                double swap = number[index_max];
+                number[index_max] = number[index_min];
+                number[index_min] = swap;
+                WriteNumbers(_filename, number);
+            }
+
+            
+        }
+    }
+
